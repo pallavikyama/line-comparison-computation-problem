2930b2b486720106b99c31337d80b31a607ffc82
pallavikyama <kyamap98@gmail.com>
2020-09-27 16:45:37 +0530

    Refactored:Using Java OOPs for line comparison

--- LineComparison.java
+++ LineComparison.java
@@ -2,28 +2,27 @@ package lineComparison;
 
 public class LineComparison {
 
+	// VARIABLES
+	double x1, y1, x2, y2;
 	float length;
+
+	public float lengthCal() {
+
+		// LINE LENGTHS EQUALITY CHECK
+		x1 = Math.random();
+		y1 = Math.random();
+		x2 = Math.random();
+		y2 = Math.random();
+		return (float) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to LINE COMPARISON COMPUTATION PROGRAM.\n");
 
-		// VARIABLES
-		double x1, y1, x2, y2;
-
 		LineComparison line1 = new LineComparison();
 		LineComparison line2 = new LineComparison();
-
-		// LINE LENGTHS EQUALITY CHECK
-		for (int count = 1; count <= 2; count++) {
-			x1 = Math.random();
-			y1 = Math.random();
-			x2 = Math.random();
-			y2 = Math.random();
-			if (count == 1)
-				line1.length = (float) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-			else
-				line2.length = (float) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-		}
+		line1.length = line1.lengthCal();
+		line2.length = line2.lengthCal();
 		System.out.println("Line1 length is " + line1.length + " units.");
 		System.out.println("Line1 length is " + line2.length + " units.");
 		System.out.println("\nComparing the two lines : " + Float.compare(line1.length, line2.length));
@@ -31,7 +30,7 @@ public class LineComparison {
 			System.out.println("Line1 is longer than line2.");
 		else if (Float.compare(line1.length, line2.length) < 0)
 			System.out.println("Line1 is shorter than line2.");
-		else
+		else if (line1.equals(line2) == true)
 			System.out.println("Line1 and line2 are of equal length.");
 	}
 }
